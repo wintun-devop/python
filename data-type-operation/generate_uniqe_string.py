13253e3cdbdb5a38544dd89080cb217faef0b867
Win Tun Hlaing <71956577+wintun-devop@users.noreply.github.com>
2025-01-17 13:57:32 +0630

    Update generate_uniqe_string.py

--- data-type-operation/generate_uniqe_string.py
+++ data-type-operation/generate_uniqe_string.py
@@ -1,1 +1,11 @@
+import random
+import string
 
+def generate_unique_string() -> str:
+    length = 30
+    chars = string.ascii_letters + string.digits
+    result = ''.join(random.choice(chars) for _ in range(length))
+    return result
+
+unique_string = generate_unique_string()
+print(unique_string)
